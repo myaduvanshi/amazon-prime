c252e11c49952a574e90a164e7342cb114baf24b
myaduvanshi <mmukesh1508@gmail.com>
2024-01-02 05:54:56 +0530

    modified

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SlidetopMain from './SlidetopMain'
-import MainContent from './MainContent/MainContent'
+import CardsContent from './CardsContent/CardsContent'
 
 function Home() {
   return (
     <>
     <div className='mx-auto w-full max-w-7xl bg-[#E3E6E6]'>
     <SlidetopMain/>
-    <MainContent/>
+    <CardsContent/>
     </div>
     </>
   )
